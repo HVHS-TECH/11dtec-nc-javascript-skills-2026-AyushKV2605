62d2620e8005548de209084668de663d2103cd8b
AyushKV2605 <24249ak@hvhs.school.nz>
2026-04-29 02:47:41 +0000

    Apr 29, 2026, 2:47 AM

--- t03_maths.js
+++ t03_maths.js
@@ -8,12 +8,16 @@ console.log( "Running T1-intro.js");
  let year = 2026;
  let age = 14;
  let money = 10;
+ let birthyear;
+ let oldage;
+ oldeage = age + 10;
+ birthyear = year + age;
 
 /****************************
  Main Code
  ****************************/
-console.log("Hi "+ username +" as of " + year + " you are " + age + " years old. You have " + money + " dollars ")
-
+console.log("Hi "+ username +" as of " + year + " you are " + age + " years old.")
+console.log()
 
 
 
