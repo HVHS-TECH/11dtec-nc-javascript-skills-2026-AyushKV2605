52cd5e0dd936c9ad80c91b4261412d259d1fd515
AyushKV2605 <24249ak@hvhs.school.nz>
2026-04-29 02:37:53 +0000

    Apr 29, 2026, 2:37 AM

---
+++ t03_maths.js
@@ -0,0 +1,21 @@
+/****************************
+Name of Task:
+****************************/
+console.log( "Running T1-intro.js");
+
+//Variables
+ let username =" Ayush ";
+ let year = 2026;
+ let age = 14;
+ let money = 10;
+
+/****************************
+ Main Code
+ ****************************/
+console.log("Hi "+ username +" as of " + year + " you are " + age + " years old. You have " + money + " dollars ")
+
+
+
+ /****************************
+  Functions
+   ****************************/
