7cde0a7cfefe4e79ed58db3bb0ce54caa162a2f5
AyushKV2605 <24249ak@hvhs.school.nz>
2026-05-03 22:18:26 +0000

    May 3, 2026, 10:18 PM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -1,1 +1,32 @@
-/* Paste your code from task 3, Maths here */
+/****************************
+Name of Task: Maths
+****************************/
+console.log( "Running T1-intro.js");
+
+//Variables
+ let username =" Ayush ";
+ let year = 2026;
+ let age = 15;
+ let money = 10;
+ let birthyear;
+ let oldage;
+ oldAge = age + 10;
+ birthyear = year - age;
+ halfmoney = money / 2
+
+/****************************
+ Main Code
+ ****************************/
+console.log("Hi "+ username +" as of " + year + " you are " + age + " years old.")
+console.log("You were born in " + birthyear)
+console.log("In 10 years you will be " + oldAge + " years old")
+console.log("You have "+ money +" dollars")
+console.log(" You spend half your money, now you have "+  halfmoney + " dollars")
+console.log("Then you get $3, now you have " + (halfmoney + 3) +" dollars")
+
+
+
+
+ /****************************
+  Functions
+   ****************************/
