ef07415bb0b9e2456517c169cdf8a0da8b2f67b1
AyushKV2605 <24249ak@hvhs.school.nz>
2026-05-03 23:05:51 +0000

    May 3, 2026, 11:05 PM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -24,7 +24,7 @@ OUTPUT.innerHTML += "<p>You were born in " + birthyear +"</p>";
 OUTPUT.innerHTML += "<p>In 10 years you will be " + oldAge + " years old</p>";
 OUTPUT.innerHTML += "<p>You have "+ money +" dollars</p>";
 OUTPUT.innerHTML += "<p> You spend half your money, now you have " +  halfmoney + " dollars</p>";
-OUTPUT.innerHTML += "<p>Then you get $3, now you have " + (halfmoney + 3) + " dollars</p>";
+OUTPUT.innerHTML += "<p>Then you get $3, now you have " + "(halfmoney + 3)" + " dollars</p>";
 
 
 
