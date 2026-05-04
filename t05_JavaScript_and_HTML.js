82bf88d42e06f9b7e3caf993d59f7a29e08a507e
AyushKV2605 <24249ak@hvhs.school.nz>
2026-05-04 23:40:10 +0000

    May 4, 2026, 11:40 PM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -13,6 +13,7 @@ console.log( "Running t05_javascriptandHtml.js");
  oldAge = age + 10;
  birthyear = year - age
  halfmoney = money / 2
+ plusMoney = halfmoney + 3
  const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
  OUTPUT.innerHTML = "<h2>Added by JavaScript</h2>";
 
@@ -24,7 +25,7 @@ OUTPUT.innerHTML += "<p>You were born in " + birthyear +"</p>";
 OUTPUT.innerHTML += "<p>In 10 years you will be " + oldAge + " years old</p>";
 OUTPUT.innerHTML += "<p>You have "+ money +" dollars</p>";
 OUTPUT.innerHTML += "<p> You spend half your money, now you have " +  halfmoney + " dollars</p>";
-OUTPUT.innerHTML += "<p>Then you get $3, now you have " + "(halfmoney + 3)" + " dollars</p>";
+OUTPUT.innerHTML += "<p>Then you get $3, now you have " + plusMoney + " dollars</p>";
 
 
 
--- t06_functions.js
+++ t06_functions.js
@@ -1,1 +1,36 @@
-/* Paste your code from task 5 here */
+/****************************
+Name of Task: Maths
+****************************/
+console.log( "Running t05_javascriptandHtml.js");
+
+//Variables
+ let username =" Ayush ";
+ let year = 2026;
+ let age = 15;
+ let money = 10;
+ let birthyear;
+ let oldage;
+ oldAge = age + 10;
+ birthyear = year - age
+ halfmoney = money / 2
+ plusMoney = halfmoney + 3
+ const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+ OUTPUT.innerHTML = "<h2>Added by JavaScript</h2>";
+
+/****************************
+ Main Code
+ ****************************/
+OUTPUT.innerHTML += "<p>Hi " + username + " as of " + year + " you are " + age + " years old.</p>";
+OUTPUT.innerHTML += "<p>You were born in " + birthyear +"</p>";
+OUTPUT.innerHTML += "<p>In 10 years you will be " + oldAge + " years old</p>";
+OUTPUT.innerHTML += "<p>You have "+ money +" dollars</p>";
+OUTPUT.innerHTML += "<p> You spend half your money, now you have " +  halfmoney + " dollars</p>";
+OUTPUT.innerHTML += "<p>Then you get $3, now you have " + plusMoney + " dollars</p>";
+
+
+
+
+ /****************************
+  Functions
+   ****************************/
+  
