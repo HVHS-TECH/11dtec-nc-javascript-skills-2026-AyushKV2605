81a298755fb774360e8eed0b1b452ad0730a9b3a
AyushKV2605 <24249ak@hvhs.school.nz>
2026-05-10 22:21:20 +0000

    May 10, 2026, 10:21 PM

--- t10_input_from_HTML.js
+++ t10_input_from_HTML.js
@@ -1,1 +1,15 @@
-/* Paste your code from the last task */
+console.log("Running Task 09...");
+
+const output = document.getElementById("spaceForJavaScriptOutput");
+
+/****************************
+ Main Code
+****************************/
+
+/****************************
+ Functions
+****************************/
+function start() {
+  console.log("Button pressed!");
+
+  output.innerHTML = "<p>You pressed the button!</p>";}
