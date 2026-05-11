af935f0ccb757d513e1fe8f83692da48a2470055
AyushKV2605 <24249ak@hvhs.school.nz>
2026-05-11 23:43:38 +0000

    May 11, 2026, 11:43 PM

--- t10_input_from_HTML.js
+++ t10_input_from_HTML.js
@@ -1,22 +1,30 @@
 console.log("Running Task 10...");
 
 
-
 /****************************
  Main Code
 ****************************/
-const OUTPUT = document.getElementById("JavaScriptOutput");
-OUTPUT.innerHTML = "Hello World!<br>"
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+
+OUTPUT.innerHTML = "Hello World!<br>";
 
 /****************************
  Functions
 ****************************/
 function start() {
   console.log("Button pressed!");
- output.innerHTML = "<p>You pressed the button!</p>";}
 
-function getFormInput(){
-    const NAME_FIELD = document.getElementById("nameField");
-    let userName = NAME_FIELD.value;
-    OUTPUT.innerHTML = "<p>Your name is "+useName+"</p>;
+  OUTPUT.innerHTML = "<p>You pressed the button!</p>";
+}
+
+
+function getFormInput() {
+
+  const NAME_FIELD = document.getElementById("namefield");
+
+  let userName = NAME_FIELD.value;
+
+  console.log(userName);
+
+  OUTPUT.innerHTML = "<p>Your name is " + userName + "</p>";
 }
