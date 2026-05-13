7c3a27ffa0eaee2b771515ad4d759bc4a55012f5
AyushKV2605 <24249ak@hvhs.school.nz>
2026-05-13 02:20:26 +0000

    May 13, 2026, 2:20 AM

--- t10_input_from_HTML.js
+++ t10_input_from_HTML.js
@@ -1,21 +1,37 @@
-console.log("Running Task 10...");
+* Paste/*********************************
+  name of task: Task 10 - Input from HTML
+*********************************/
+console.log("t10_input_from_HTML.js");
 
+/*********************************
+  main code
+*********************************/
 
+function runTask10() {
 
-/****************************
- Main Code
-****************************/
-const OUTPUT = document.getElementById("spaceForJavaScriptOutput")
-OUTPUT.innerHTML = "Hello World!<br>"
+  const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 
-/****************************
- Functions
-****************************/
-function start() {
-  console.log("Button pressed!");
- output.innerHTML = "<p>You pressed the button!</p>";}
- function getFormInput() {
-    const nameField = document.getElementById("namefield");
-    let username = nameField.value;
-    OUTPUT.innerHTML = "<p>Your name is "+username+"</p>";
- }
+  // get values from the form
+  const NAME_FIELD = document.getElementById("nameField");
+  const AGE_FIELD = document.getElementById("ageField");
+  const MONEY_FIELD = document.getElementById("moneyField");
+  const YEAR_FIELD = document.getElementById("yearField");
+
+  let username = NAME_FIELD.value;
+  let age = Number(AGE_FIELD.value);
+  let money = Number(MONEY_FIELD.value);
+  let year = Number(YEAR_FIELD.value);
+
+  // calculations
+  let birthYear = year - age;
+  let oldAge = age + 10;
+  let extraMoney = money + 3;
+
+  // output to HTML
+  OUTPUT.innerHTML = "<p>Hi " + username + " as of " + year + " you are " + age + " years old. You have " + money + " dollars.</p>";
+  OUTPUT.innerHTML += "<p>You were born in " + birthYear + "</p>";
+  OUTPUT.innerHTML += "<p>In ten years you will be " + oldAge + "</p>";
+  OUTPUT.innerHTML += "<p>You have " + money + " dollars</p>";
+  OUTPUT.innerHTML += "<p>You spend half your money, you now have " + money/2 + " dollars</p>";
+  OUTPUT.innerHTML += "<p>You get 3 dollars, now you have " + extraMoney + " dollars</p>";
+}
