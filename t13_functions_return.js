8fcefa4ca61b7d5abdc250a162a540b62fecab9e
AyushKV2605 <24249ak@hvhs.school.nz>
2026-05-27 02:38:58 +0000

    May 27, 2026, 2:38 AM

--- t13_functions_return.js
+++ t13_functions_return.js
@@ -1,4 +1,3 @@
-/*********************************/
 console.log("t11_input_from_HTML.js");
 
 /*********************************
@@ -30,45 +29,44 @@ function runTask11() {
   OUTPUT.innerHTML += "<p>You were born in " + birthYear + "</p>";
   OUTPUT.innerHTML += "<p>In ten years you will be " + oldAge + "</p>";
   OUTPUT.innerHTML += "<p>You have " + money + " dollars</p>";
-  OUTPUT.innerHTML += "<p>You spend half your money, you now have " + money/2 + " dollars</p>";
+  OUTPUT.innerHTML += "<p>You spend half your money, you now have " + money / 2 + " dollars</p>";
   OUTPUT.innerHTML += "<p>You get 3 dollars, now you have " + extraMoney + " dollars</p>";
+
+  let mondayPay = calculatePay(8);
+  let tuesdayPay = calculatePay(6);
+
+  OUTPUT.innerHTML += "<br>This week's pay:<br>";
+  OUTPUT.innerHTML += "Monday: $" + mondayPay + "<br>";
+  OUTPUT.innerHTML += "Tuesday: $" + tuesdayPay + "<br>";
 }
 
 let name = "Ayush";
 let age = 15;
 let isAwake = true;
 
-
-
 var num1 = "50";
-num1+num1;
+num1 + num1;
 
 var num2 = "50";
-num2+num2;
+num2 + num2;
 
 num1 = Number(num1);
-num1+num1;
+num1 + num1;
 
-if (age < 15){
-    console.log("Defininetaly too young");
+if (age < 15) {
+    console.log("Definitely too young");
 }
-if  (age >= 15){    
+
+if (age >= 15) {
     console.log("Might be too old");
 }
-if (age === 15){
+
+if (age === 15) {
     console.log("Just right!");
 }
 
-let mondayPay = calculatePay(8);
-let tuesdayPay = calculatePay(6);
-
-
-OUTPUT.innerHTML += "This week's pay:<br>";
-OUTPUT.innerHTML += "Monday: $" + mondayPay + "<br>";
-OUTPUT.innerHTML += "Tuesday: $" + tuesdayPay + "<br>";
-
-function calculatePay(_hours){
-    let pay = _hours * 18.80; 
+function calculatePay(_hours) {
+    let pay = _hours * 18.80;
     pay = pay - (pay * 0.105);
     pay = pay - (pay * 0.04);
     pay = pay + 10;
